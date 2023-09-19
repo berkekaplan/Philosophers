5ceab75a00071de7d95502cb476fa76d162fb163
berkekaplan <berkekaplan41@gmail.com>
2023-09-19 15:54:54 +0300

    42

--- philo/arg_opr.c
+++ philo/arg_opr.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   arg_opr.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkaplan <@student.42kocaeli.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/19 15:27:45 by mkaplan           #+#    #+#             */
-/*   Updated: 2023/09/19 15:36:06 by mkaplan          ###   ########.fr       */
+/*   Created: 2023/09/19 15:45:12 by mkaplan           #+#    #+#             */
+/*   Updated: 2023/09/19 15:45:13 by mkaplan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int argc, char **argv)
+void    argument_placer(int argc, char **argv, argv_t *philo)
 {
-    u_int64_t   time;
-    philo_t     phil;
-
-    if(argc != 5 || argc != 6)
-        return 0;
-
-    phil.philos = (argv_t *)malloc(sizeof(argv_t));
-    argument_placer(argc, argv, phil.philos);
     
 }
---
+++ philo/philo.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaplan <@student.42kocaeli.com.tr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 15:17:50 by mkaplan           #+#    #+#             */
+/*   Updated: 2023/09/19 15:20:07 by mkaplan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <pthread.h>
+
+# define TAKEN_FORK "has taken a fork"
+# define IS_EATING "is eating"
+# define IS_SLEEPING "is sleeping"
+# define IS_THINKING "is thinking"
+# define DIE "died"
+
+typedef struct argv
+{
+    int         number_of_philosophers;
+    int         forks;
+    int         number_of_times_each_philosopher_must_eat;
+    u_int64_t   time_to_die;
+    u_int64_t   time_to_eat;
+    u_int64_t   time_to_sleep;
+}       argv_t;
+
+typedef struct philo
+{
+    u_int64_t   start_time;
+    argv_t        *philos;
+}       philo_t;
+
+#endif
